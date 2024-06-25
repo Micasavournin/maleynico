eef19f7552e149836fdb25114b1824b3c939eec9
Mica Savournin <micaasavoo@gmail.com>
2024-06-24 22:49:12 -0300

    f

--- src/components/Page6.jsx
+++ src/components/Page6.jsx
@@ -1,7 +1,7 @@
 const Page6 = () => {
     return (
         <div className="contenedor">
-                <img src="https://firebasestorage.googleapis.com/v0/b/tarjeta-invitacion-2f4f0.appspot.com/o/tarjeta%201%20(BODA)-07.jpg?alt=media&token=bc17604f-e36c-4c4f-9abc-6b83edd78277" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+                <img src="https://firebasestorage.googleapis.com/v0/b/tarjeta-invitacion-2f4f0.appspot.com/o/regalito-07.jpg?alt=media&token=ddf10619-8aea-41ed-8702-2342776ff2f8" alt="" style={{ maxWidth: '100%', maxHeight: '100%' }} />
             
         </div>
         )
